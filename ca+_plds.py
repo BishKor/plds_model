2e5c7b0d652ed8bea35f7c3bea3170f8d2188ad7
Bishara Korkor <Bishara.Korkor@gmail.com>
2017-10-02 18:16:46 -0700

    complete code, not debugged yet

--- ca+_plds.py
+++ ca+_plds.py
@@ -4,20 +4,19 @@ import numdifftools as nd
 from newton_method import nr_algo
 
 
-def logposterior(y, C, d, A, q, q0, G, u, num_time_steps, num_neurons):
+def logposterior(y, C, d, A, q, q0, G, u, n_time_steps, n_trials, n_neurons):
     """
-    :param x: latent space
     :param y: neuron capture data
     :param C: latent space to neuron space transformation matrix
     :param d: mean firing rates
     :param A: deterministic component of the evolution state[t] to state[t+1]
-    :param Q: covariance of the innovations that perturb the latent state at each time step
-    :param Q0: covariance of the initial state x1 of each trial
+    :param q: covariance of the innovations that perturb the latent state at each time step
+    :param q0: covariance of the initial state x1 of each trial
     :param G: mapping of stimuli to "latent space stimuli"
     :param u: stimuli (4-D one-hot)
-    :param T: number of time steps
-    :param K: number of trials
-    :param q: number of neurons
+    :param n_time_steps: number of time steps
+    :param n_trials: number of trials
+    :param n_neurons: number of neurons
     :return: the log-posterior of eq.4 in Macke et al. 2011
     """
 
@@ -30,15 +29,15 @@ def logposterior(y, C, d, A, q, q0, G, u, num_time_steps, num_neurons):
         term1 = 0
         term2 = 0
         term3 = 0
-        constants = .5 * (num_time_steps-1) * np.log(q) + .5 * np.log(q0)
+        constants = .5 * (n_time_steps-1) * np.log(q) + .5 * np.log(q0)
 
-        for k in range(num_neurons):
-            for t in range(num_time_steps):
+        for k in range(n_neurons):
+            for t in range(n_time_steps):
                 term1 += y[k, t].T @ (C @ x[k, t] + d) + np.sum(-np.exp(C @ x[k, t] + d))
 
             term2 += -np.dot(np.dot(x[k, 1] - x[0], q0inv), x[k, 1] - x[0])
 
-            for t in range(T-1):
+            for t in range(n_time_steps-1):
                 alp = x[k, t+1] - np.dot(A, x[k, t]) - np.dot(G, u[k, t])
                 term3 += np.dot(np.dot(alp.T, qinv), alp)
 
@@ -47,15 +46,15 @@ def logposterior(y, C, d, A, q, q0, G, u, num_time_steps, num_neurons):
     return logpost
 
 
-def jointloglikelihood(y, num_neurons, num_time_steps, C, mu, d, cov, Q, Q0, x, A, u, G):
+def jointloglikelihood(y, n_neurons, n_time_steps, C, mu, d, cov, Q, Q0, x, A, u, G):
 
     # precompute for efficiency
     q0inv = np.inv(Q0)
     qinv = np.inv(Q)
 
     jll = 0
-    for k in range(num_neurons):
-        for t in range(num_time_steps-1):
+    for k in range(n_neurons):
+        for t in range(n_time_steps-1):
             jll += y[k, t].T @ C @ mu[t] + y[k, t].T @ d \
                    - .5 * np.exp(C @ mu[t] + .5 * C.T @ cov[t, t] @ C + d) \
                    - .5 * mu[1].T @ q0inv @ mu[1] + .5 * np.trace(q0inv @ cov[1, 1]) \
@@ -72,11 +71,11 @@ def jointloglikelihood(y, num_neurons, num_time_steps, C, mu, d, cov, Q, Q0, x, 
                    + .5 * u[t].T @ G.T @ qinv @ mu[t + 1] \
                    - .5 * u[t].T @ G.T @ qinv @ A @ mu[t + 1] \
                    - .5 * u[t].T @ G.T @ qinv @ G @ u[t]
-        jll += -.5 * np.log(q0inv) - .5 * (T-1) * np.log(q0inv)
+        jll += -.5 * np.log(q0inv) - .5 * (n_time_steps-1) * np.log(q0inv)
     return jll
 
 
-def jllmake(y, num_neurons, num_time_steps, mu, cov, Q, Q0, x0, A, u, G):
+def jllmake(y, n_neurons, n_time_steps, mu, cov, Q, Q0, x0, A, u, G):
     def jointloglikelihood(C, d):
 
         # precompute for efficiency
@@ -84,8 +83,8 @@ def jllmake(y, num_neurons, num_time_steps, mu, cov, Q, Q0, x0, A, u, G):
         qinv = np.inv(Q)
 
         jll = 0
-        for k in range(num_neurons):
-            for t in range(num_time_steps-1):
+        for k in range(n_neurons):
+            for t in range(n_time_steps-1):
                 jll += y[k, t].T @ C @ mu[t] + y[k, t].T @ d \
                        - .5 * np.exp(C @ mu[t] + .5 * C.T @ cov[t, t] @ C + d) \
                        - .5 * mu[1].T @ q0inv @ mu[1] + .5 * np.trace(q0inv @ cov[1, 1]) \
@@ -102,7 +101,7 @@ def jllmake(y, num_neurons, num_time_steps, mu, cov, Q, Q0, x0, A, u, G):
                        + .5 * u[t].T @ G.T @ qinv @ mu[t + 1] \
                        - .5 * u[t].T @ G.T @ qinv @ A @ mu[t + 1] \
                        - .5 * u[t].T @ G.T @ qinv @ G @ u[t]
-            jll += -.5 * np.log(q0inv) - .5 * (T-1) * np.log(q0inv)
+            jll += -.5 * np.log(q0inv) - .5 * (n_time_steps-1) * np.log(q0inv)
         return jll
     return jointloglikelihood
 
@@ -117,24 +116,75 @@ def laplace_approximation(f, mu0):
 
 if __name__ == "__main__":
     # load data
+    frameHz = .1 # seconds
     data = scio.loadmat('data/compiled_dF033016.mat')
-    neurons = data['behavdF']
+    y = data['behavdF']
+    onset = data['onsetFrame']
+    resptime = data['resptime']
+    correct = data['correct']
     orient = np.array(data['orient'], np.int)
     location = np.array(data['location'] > 0, np.int)
-    stimuli = np.array([o*l, (1-o)*l, o*(1-l), (1-o)*(1-l)] for o, l in zip(orient, location))
-    num_time_steps = 30  # number of time steps
-    num_trials = 372  # number of trials
-    num_neurons = 300  # number of neurons
+    n_time_steps = 26187
+    n_trials = 1  # number of trials
+    n_neurons = 300  # number of neurons
+    n_latent_dims = 8
+    stimuli_dims = 4
 
-    # perform laplace approximation on log-posterior with Newton-Raphson optimization to find mean and covariance
-    mu, cov = laplace_approximation()
+    # create empty u
+    u = np.zeros((n_latent_dims, n_time_steps))
 
-    # Use analytic expressions to compute parameters x0, Q, Q0, A, G
+    for ot, rt, cor, ori, loc in zip(onset, resptime, correct, orient, location):
+        stimuli = np.array([ori*loc, (1-ori)*loc, ori*(1-loc), (1-ori)*(1-loc)])
+        # compute what u should be here
+        u[ot:ot+int((rt+2.75+(4.85-2.75)*(1-cor))*frameHz)] = np.array([1, 1, 1, 1])
 
-    # Create joint log posterior with determined parameters
-    jll = jllmake(y, num_neurons, num_time_steps, mu, cov, Q, Q0, x0, A, u, G)
+    # Initialize parameters to random values
+    C = np.zeros((n_neurons, n_latent_dims))
+    d = np.zeros(n_neurons)
+    x0 = np.zeros(n_latent_dims)
+    A = np.zeros((n_latent_dims, n_latent_dims))
+    Q0 = np.zeros((n_latent_dims, n_latent_dims))
+    Q = np.zeros((n_latent_dims, n_latent_dims))
+    G = np.zeros((n_latent_dims, stimuli_dims))
+    B = np.zeros(n_latent_dims)
 
-    # Second NR minimization to compute C, d (and in principle, D)
-    C0
-    d0
-    C, d = nr_algo(jll, [C0, d0])
+    max_epochs = 1000
+    for epoch in max_epochs:
+        # Create instance of log posterior
+        f = logposterior(y, C, d, A, Q, Q0, G, u, n_time_steps, n_neurons)
+        # perform laplace approximation on log-posterior with Newton-Raphson optimization to find mean and covariance
+        mu, cov = laplace_approximation(f, (mu, cov))
+
+        # Use analytic expressions to compute parameters x0, Q, Q0, A, G
+        x0 = mu[0:n_latent_dims]
+        q0 = cov[0:n_latent_dims, 0:n_latent_dims]
+
+        A = sum(cov[(t+1)*n_latent_dims, t] + np.outer(mu[(t+1)*n_latent_dims], mu[t*n_latent_dims].T) for t in range(n_time_steps - 1)) @ \
+            np.inv(sum(cov[t*n_latent_dims:(t+1)*n_latent_dims, t*n_latent_dims:(t+1)*n_latent_dims] +
+                    np.dot(mu[t*n_latent_dims], mu[t*n_latent_dims].T) for t in range(n_time_steps - 1)))
+
+        q = sum(cov[(t + 1)*n_latent_dims:(t + 2)*n_latent_dims, (t + 1)*n_latent_dims:(t + 2)*n_latent_dims] +
+                    np.outer(mu[(t + 1)*n_latent_dims], mu[(t + 1)*n_latent_dims].T) -
+                    (cov[(t + 1)*n_latent_dims:(t + 2)*n_latent_dims, t*n_latent_dims:(t + 1)*n_latent_dims] +
+                    np.outer(mu[(t + 1)*n_latent_dims], mu[t*n_latent_dims])) @ A.T -
+                    np.outer(mu[(t + 1)*n_latent_dims], u[t*n_latent_dims].T) @ B.T +
+                    A @ (cov[t*n_latent_dims:(t + 1)*n_latent_dims, (t + 1)*n_latent_dims:(t + 2)*n_latent_dims] +
+                         mu[t*n_latent_dims] @ mu[(t + 1)*n_latent_dims].T) +
+                    A @ (cov[t*n_latent_dims:(t + 1)*n_latent_dims, t*n_latent_dims:(t + 1)*n_latent_dims] +
+                         np.outer(mu[t*n_latent_dims], mu[t*n_latent_dims].T)) @ A.T +
+                    A @ np.outer(mu[t*n_latent_dims], u[t*n_latent_dims].T) @ B.T -
+                    B @ np.outer(u[t*n_latent_dims], mu[t*n_latent_dims].T) +
+                    B @ np.outer(u[t*n_latent_dims], mu[t*n_latent_dims].T) @ A.T +
+                    B @ np.outer(u[t*n_latent_dims], u[t*n_latent_dims]) @ B.T
+                    for t in n_time_steps - 1)
+        B = sum(np.outer(mu[(t+1)*n_latent_dims], u[t*n_latent_dims].T) - A @ np.outer(mu[t*n_latent_dims], u[t*n_latent_dims])
+                for t in n_time_steps-1) @ np.inv(sum(np.outer(u[t*n_latent_dims], u[t*n_latent_dims])for t in n_time_steps-1))
+
+
+        # Create instance of joint log posterior with determined parameters
+        jll = jllmake(y, n_neurons, n_time_steps, mu, cov, Q, Q0, x0, A, u, G)
+
+        # Second NR minimization to compute C, d (and in principle, D)
+
+        C, d = nr_algo(jll, (C, d))
+
