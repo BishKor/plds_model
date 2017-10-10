e4d905c9a0989190f1c379320978cdca41f63b1f
Bishara Korkor <Bishara.Korkor@gmail.com>
2017-10-10 14:32:40 -0700

    mid tuesday commit

--- ca+_plds.py
+++ ca+_plds.py
@@ -4,7 +4,7 @@ import numdifftools as nd
 from newton_method import nr_algo
 
 
-def logposterior(y, C, d, A, q, q0, G, u, n_time_steps, n_trials, n_neurons):
+def logposterior(y, C, d, A, B, q, q0, u, n_time_steps, n_neurons):
     """
     :param y: neuron capture data
     :param C: latent space to neuron space transformation matrix
@@ -12,10 +12,9 @@ def logposterior(y, C, d, A, q, q0, G, u, n_time_steps, n_trials, n_neurons):
     :param A: deterministic component of the evolution state[t] to state[t+1]
     :param q: covariance of the innovations that perturb the latent state at each time step
     :param q0: covariance of the initial state x1 of each trial
-    :param G: mapping of stimuli to "latent space stimuli"
+    :param B: mapping of stimuli to "latent space stimuli"
     :param u: stimuli (4-D one-hot)
     :param n_time_steps: number of time steps
-    :param n_trials: number of trials
     :param n_neurons: number of neurons
     :return: the log-posterior of eq.4 in Macke et al. 2011
     """
@@ -23,168 +22,227 @@ def logposterior(y, C, d, A, q, q0, G, u, n_time_steps, n_trials, n_neurons):
     def logpost(x):
 
         # first compute useful values
-        q0inv = np.inv(q0)
-        qinv = np.inv(q)
-
-        term1 = 0
-        term2 = 0
-        term3 = 0
+        q0inv = np.linalg.inv(q0)
+        qinv = np.linalg.inv(q)
         constants = .5 * (n_time_steps-1) * np.log(q) + .5 * np.log(q0)
+        term1 = sum(y[t*n_neurons:(t+1)*n_neurons].T @ (C @ x[t*nld:(t+1)*nld] + d) +
+                    np.sum(-np.exp(C @ x[t*nld:(t+1)*nld] + d)) for t in range(n_time_steps-1))
+        term2 = - .5 * (x[1] - x[0]).T @ q0inv @ (x[1] - x[0])
 
-        for k in range(n_neurons):
-            for t in range(n_time_steps):
-                term1 += y[k, t].T @ (C @ x[k, t] + d) + np.sum(-np.exp(C @ x[k, t] + d))
-
-            term2 += -np.dot(np.dot(x[k, 1] - x[0], q0inv), x[k, 1] - x[0])
-
-            for t in range(n_time_steps-1):
-                alp = x[k, t+1] - np.dot(A, x[k, t]) - np.dot(G, u[k, t])
-                term3 += np.dot(np.dot(alp.T, qinv), alp)
+        term3 = - .5 * sum((x[(t + 1) * nld:(t + 2) * nld] -
+                    A @ x[t * nld:(t + 1) * nld] -
+                    B @ u[t*nld:(t+1)*nld]).T @ qinv @
+                    (x[(t + 1) * nld:(t + 2) * nld] -
+                    A @ x[t * nld:(t + 1) * nld] -
+                    B @ u[t*nld:(t+1)*nld])
+                    for t in range(n_time_steps - 1))
 
         return constants + term1 + term2 + term3
 
     return logpost
 
+#
+# def jointloglikelihood(y, n_stimuli_dims, n_neurons, n_time_steps, C, mu, d, cov, Q, Q0, x, A, u, B):
+#
+#     # precompute for efficiency
+#     q0inv = np.linalg.inv(Q0)
+#     qinv = np.linalg.inv(Q)
+#
+#     jll = 0
+#     for k in range(n_neurons):
+#         for t in range(n_time_steps-1):
+#             jll += y[k, t].T @ C @ mu[t] + y[k, t].T @ d \
+#                    - .5 * np.exp(C @ mu[t] + .5 * C.T @ cov[t, t] @ C + d) \
+#                    - .5 * mu[1].T @ q0inv @ mu[1] + .5 * np.trace(q0inv @ cov[1, 1]) \
+#                    + .5 * mu[1].T @ q0inv @ x[0] \
+#                    + .5 * x[0].T @ q0inv @ mu[1] \
+#                    - .5 * x[0].T @ q0inv @ mu[1] \
+#                    - .5 * x[0].T @ q0inv @ x[1] \
+#                    - .5 * mu[t + 1] @ q0inv @ mu[t + 1] + np.trace(qinv @ cov[t + 1, t + 1]) \
+#                    + .5 * mu[t + 1].T * qinv @ A @ mu[t] + np.trace(qinv @ A @ cov[t.t + 1]) \
+#                    + .5 * mu[t + 1].T @ qinv @ B @ u[t] \
+#                    + .5 * mu[t].T @ A.T @ qinv @ mu[t + 1] + np.trace(A @ qinv @ cov[t + 1, t]) \
+#                    - .5 * mu[t].T @ A.T @ qinv @ A @ B @ u[t] + np.trace(A.T @ qinv @ A @ cov[t, t]) \
+#                    - .5 * mu[t].T @ A.T @ qinv @ B @ u[t] \
+#                    + .5 * u[t].T @ B.T @ qinv @ mu[t + 1] \
+#                    - .5 * u[t].T @ B.T @ qinv @ A @ mu[t + 1] \
+#                    - .5 * u[t].T @ B.T @ qinv @ B @ u[t]
+#         jll += -.5 * np.log(q0inv) - .5 * (n_time_steps-1) * np.log(q0inv)
+#     return jll
 
-def jointloglikelihood(y, n_neurons, n_time_steps, C, mu, d, cov, Q, Q0, x, A, u, G):
 
-    # precompute for efficiency
-    q0inv = np.inv(Q0)
-    qinv = np.inv(Q)
+def jllmake(y, n_stimuli_dims, n_neurons, n_time_steps, mu, cov, Q, Q0, x0, A, u, B):
+    def jointloglikelihood(dC):
 
-    jll = 0
-    for k in range(n_neurons):
-        for t in range(n_time_steps-1):
-            jll += y[k, t].T @ C @ mu[t] + y[k, t].T @ d \
-                   - .5 * np.exp(C @ mu[t] + .5 * C.T @ cov[t, t] @ C + d) \
-                   - .5 * mu[1].T @ q0inv @ mu[1] + .5 * np.trace(q0inv @ cov[1, 1]) \
-                   + .5 * mu[1].T @ q0inv @ x[0] \
-                   + .5 * x[0].T @ q0inv @ mu[1] \
-                   - .5 * x[0].T @ q0inv @ mu[1] \
-                   - .5 * x[0].T @ q0inv @ x[1] \
-                   - .5 * mu[t + 1] @ q0inv @ mu[t + 1] + np.trace(qinv @ cov[t + 1, t + 1]) \
-                   + .5 * mu[t + 1].T * qinv @ A @ mu[t] + np.trace(qinv @ A @ cov[t.t + 1]) \
-                   + .5 * mu[t + 1].T @ qinv @ G @ u[t] \
-                   + .5 * mu[t].T @ A.T @ qinv @ mu[t + 1] + np.trace(A @ qinv @ cov[t + 1, t]) \
-                   - .5 * mu[t].T @ A.T @ qinv @ A @ G @ u[t] + np.trace(A.T @ qinv @ A @ cov[t, t]) \
-                   - .5 * mu[t].T @ A.T @ qinv @ G @ u[t] \
-                   + .5 * u[t].T @ G.T @ qinv @ mu[t + 1] \
-                   - .5 * u[t].T @ G.T @ qinv @ A @ mu[t + 1] \
-                   - .5 * u[t].T @ G.T @ qinv @ G @ u[t]
-        jll += -.5 * np.log(q0inv) - .5 * (n_time_steps-1) * np.log(q0inv)
-    return jll
-
-
-def jllmake(y, n_neurons, n_time_steps, mu, cov, Q, Q0, x0, A, u, G):
-    def jointloglikelihood(C, d):
-
+        d = dC[:n_neurons]
+        C = dC[n_neurons:].reshape(-1, n_neurons)
         # precompute for efficiency
-        q0inv = np.inv(Q0)
-        qinv = np.inv(Q)
+        q0inv = np.linalg.inv(Q0)
+        qinv = np.linalg.inv(Q)
 
-        jll = 0
-        for k in range(n_neurons):
-            for t in range(n_time_steps-1):
-                jll += y[k, t].T @ C @ mu[t] + y[k, t].T @ d \
-                       - .5 * np.exp(C @ mu[t] + .5 * C.T @ cov[t, t] @ C + d) \
-                       - .5 * mu[1].T @ q0inv @ mu[1] + .5 * np.trace(q0inv @ cov[1, 1]) \
-                       + .5 * mu[1].T @ q0inv @ x0 \
-                       + .5 * x0.T @ q0inv @ mu[1] \
-                       - .5 * x0.T @ q0inv @ mu[1] \
-                       - .5 * x0.T @ q0inv @ x0 \
-                       - .5 * mu[t + 1] @ q0inv @ mu[t + 1] + np.trace(qinv @ cov[t + 1, t + 1]) \
-                       + .5 * mu[t + 1].T * qinv @ A @ mu[t] + np.trace(qinv @ A @ cov[t,t + 1]) \
-                       + .5 * mu[t + 1].T @ qinv @ G @ u[t] \
-                       + .5 * mu[t].T @ A.T @ qinv @ mu[t + 1] + np.trace(A @ qinv @ cov[t + 1, t]) \
-                       - .5 * mu[t].T @ A.T @ qinv @ A @ G @ u[t] + np.trace(A.T @ qinv @ A @ cov[t, t]) \
-                       - .5 * mu[t].T @ A.T @ qinv @ G @ u[t] \
-                       + .5 * u[t].T @ G.T @ qinv @ mu[t + 1] \
-                       - .5 * u[t].T @ G.T @ qinv @ A @ mu[t + 1] \
-                       - .5 * u[t].T @ G.T @ qinv @ G @ u[t]
-            jll += -.5 * np.log(q0inv) - .5 * (n_time_steps-1) * np.log(q0inv)
+        jll = sum(y[t*n_neurons:(t+1)*n_neurons].T @ C @ mu[t*nld:(t+1)*nld] \
+                   + y[t*n_neurons:(t+1)*n_neurons].T @ d \
+                   - .5 * np.exp(C @ mu[t*nld:(t+1)*nld] +
+                                 .5 * C.T @ cov[t*n_neurons:(t+1)*n_neurons, t*n_neurons:(t+1)*n_neurons] @ C + d) \
+                   - .5 * mu[nld:2*nld].T @ q0inv @ mu[nld:2*nld] \
+                   + .5 * np.trace(q0inv @ cov[nld:2*nld, 1*nld:2*nld]) \
+                   + .5 * mu[nld:2*nld].T @ q0inv @ x0 \
+                   + .5 * x0.T @ q0inv @ mu[nld:2*nld] \
+                   - .5 * x0.T @ q0inv @ mu[nld:2*nld] \
+                   - .5 * x0.T @ q0inv @ x0 \
+                   - .5 * mu[(t+1)*nld:(t+2)*nld] @ q0inv @ mu[(t+1)*nld:(t+2)*nld] \
+                   + np.trace(qinv @ cov[(t+1)*nld, (t+1)*nld]) \
+                   + .5 * mu[(t+1)*nld:(t+2)*nld].T * qinv @ A @ mu[t*nld:(t+1)*nld] \
+                   + np.trace(qinv @ A @ cov[t*nld:(t+1)*nld, (t+1)*nld:(t+2)*nld]) \
+                   + .5 * mu[(t+1)*nld].T @ qinv @ B @ u[t*n_stimuli_dims:(t+1)*n_stimuli_dims] \
+                   + .5 * mu[t*nld:(t+1)*nld].T @ A.T @ qinv @ mu[(t+1)*nld:(t+2)*nld] \
+                   + np.trace(A @ qinv @ cov[(t+1)*nld:(t+2)*nld, t*nld:(t+1)*nld]) \
+                   - .5 * mu[t*nld:(t+1)*nld].T @ A.T @ qinv @ A @ B @ u[t*n_stimuli_dims:(t+1)*n_stimuli_dims] \
+                   + np.trace(A.T @ qinv @ A @ cov[t*nld:(t+1)*nld, t*nld:(t+1)*nld]) \
+                   - .5 * mu[t*nld:(t+1)*nld].T @ A.T @ qinv @ B @ u[t*n_stimuli_dims:(t+1)*n_stimuli_dims] \
+                   + .5 * u[t*n_stimuli_dims:(t+1)*n_stimuli_dims].T @ B.T @ qinv @ mu[(t+1)*nld:(t+2)*nld] \
+                   - .5 * u[t*n_stimuli_dims:(t+1)*n_stimuli_dims].T @ B.T @ qinv @ A @ mu[(t+1)*nld:(t+2)*nld] \
+                   - .5 * u[t*n_stimuli_dims:(t+1)*n_stimuli_dims].T @ B.T @ qinv @ B @ u[t*n_stimuli_dims:(t+1)*n_stimuli_dims] for t in range(n_time_steps-1)) \
+              - .5 * np.log(q0inv) - .5 * (n_time_steps-1) * np.log(q0inv)
+
         return jll
     return jointloglikelihood
 
 
-def laplace_approximation(f, mu0):
-    # use NR algorithm to compute minimum of jointloglikelihood
-    mu = nr_algo(f, mu0)
+def laplace_approximation(f, mu):
+    # use NR algorithm to compute minimum of log-likelihood
+    df = nd.Gradient(f)
+    H = nd.Hessian(f)
+    # compute hessian of log-likelihood
+    mu = nr_algo(f, mu, df, H)
+
     # negative inverse of Hessian is covariance matrix
-    covariance = -np.inv(nd.Hessian(f)(mu0))
+    covariance = -np.linalg.inv(H(mu))
     return mu, covariance
 
 
+def jllHessian():
+    return None
+
+
+def jllDerivative():
+    return None
+
 if __name__ == "__main__":
     # load data
-    frameHz = .1 # seconds
-    data = scio.loadmat('data/compiled_dF033016.mat')
-    y = data['behavdF']
-    onset = data['onsetFrame']
-    resptime = data['resptime']
-    correct = data['correct']
-    orient = np.array(data['orient'], np.int)
-    location = np.array(data['location'] > 0, np.int)
     n_time_steps = 26187
-    n_trials = 1  # number of trials
     n_neurons = 300  # number of neurons
-    n_latent_dims = 8
-    stimuli_dims = 4
+    nld = 8 # number of latent dimensions
+    n_stimuli_dims = 4
+    frameHz = 10  # frames per seconds
+    data = scio.loadmat('data/compiled_dF033016.mat')
+    y = data['behavdF'][0].flatten()
+    onset = data['onsetFrame'].T[0]
+    resptime = data['resptime'].T[0]
+    correct = data['correct'][0]
+    orient = np.array(data['orient'][0], np.int)
+    location = (data['location'][0]+1)//2
 
     # create empty u
-    u = np.zeros((n_latent_dims, n_time_steps))
-
+    u = np.zeros((n_time_steps, n_stimuli_dims))
+    # set stimuli
     for ot, rt, cor, ori, loc in zip(onset, resptime, correct, orient, location):
-        stimuli = np.array([ori*loc, (1-ori)*loc, ori*(1-loc), (1-ori)*(1-loc)])
         # compute what u should be here
-        u[ot:ot+int((rt+2.75+(4.85-2.75)*(1-cor))*frameHz)] = np.array([1, 1, 1, 1])
+        u[ot:ot+int((rt+2.75+(4.85-2.75)*(1-cor))*frameHz)] = \
+            np.array([ori*loc, (1-ori)*loc, ori*(1-loc), (1-ori)*(1-loc)], np.int)
 
     # Initialize parameters to random values
-    C = np.zeros((n_neurons, n_latent_dims))
-    d = np.zeros(n_neurons)
-    x0 = np.zeros(n_latent_dims)
-    A = np.zeros((n_latent_dims, n_latent_dims))
-    Q0 = np.zeros((n_latent_dims, n_latent_dims))
-    Q = np.zeros((n_latent_dims, n_latent_dims))
-    G = np.zeros((n_latent_dims, stimuli_dims))
-    B = np.zeros(n_latent_dims)
+    C = np.empty((n_neurons, nld))
+    d = np.empty(n_neurons)
+    x0 = np.empty(nld)
+    A = np.empty((nld, nld))
+    Q0 = np.empty((nld, nld))
+    Q = np.empty((nld, nld))
+    B = np.empty((nld, n_stimuli_dims))
+    mu = np.empty(nld*n_time_steps)
+    cov = np.empty((nld, nld))
 
     max_epochs = 1000
-    for epoch in max_epochs:
-        # Create instance of log posterior
-        f = logposterior(y, C, d, A, Q, Q0, G, u, n_time_steps, n_neurons)
+    for epoch in range(max_epochs):
         # perform laplace approximation on log-posterior with Newton-Raphson optimization to find mean and covariance
-        mu, cov = laplace_approximation(f, (mu, cov))
+        mu, cov = laplace_approximation(logposterior(y, C, d, A, B, Q, Q0, u, n_time_steps, n_neurons), mu)
 
-        # Use analytic expressions to compute parameters x0, Q, Q0, A, G
-        x0 = mu[0:n_latent_dims]
-        q0 = cov[0:n_latent_dims, 0:n_latent_dims]
+        # Use analytic expressions to compute parameters x0, Q, Q0, A, B
+        x0 = mu[0:nld]
+        q0 = cov[0:nld, 0:nld]
 
-        A = sum(cov[(t+1)*n_latent_dims, t] + np.outer(mu[(t+1)*n_latent_dims], mu[t*n_latent_dims].T) for t in range(n_time_steps - 1)) @ \
-            np.inv(sum(cov[t*n_latent_dims:(t+1)*n_latent_dims, t*n_latent_dims:(t+1)*n_latent_dims] +
-                    np.dot(mu[t*n_latent_dims], mu[t*n_latent_dims].T) for t in range(n_time_steps - 1)))
+        A = sum(cov[(t+1)*nld, t] + np.outer(mu[(t+1)*nld], mu[t*nld].T) for t in range(n_time_steps - 1)) @ \
+            np.linalg.inv(sum(cov[t*nld:(t+1)*nld, t*nld:(t+1)*nld] +
+            mu[t*nld] @ mu[t*nld].T for t in range(n_time_steps - 1)))
 
-        q = sum(cov[(t + 1)*n_latent_dims:(t + 2)*n_latent_dims, (t + 1)*n_latent_dims:(t + 2)*n_latent_dims] +
-                    np.outer(mu[(t + 1)*n_latent_dims], mu[(t + 1)*n_latent_dims].T) -
-                    (cov[(t + 1)*n_latent_dims:(t + 2)*n_latent_dims, t*n_latent_dims:(t + 1)*n_latent_dims] +
-                    np.outer(mu[(t + 1)*n_latent_dims], mu[t*n_latent_dims])) @ A.T -
-                    np.outer(mu[(t + 1)*n_latent_dims], u[t*n_latent_dims].T) @ B.T +
-                    A @ (cov[t*n_latent_dims:(t + 1)*n_latent_dims, (t + 1)*n_latent_dims:(t + 2)*n_latent_dims] +
-                         mu[t*n_latent_dims] @ mu[(t + 1)*n_latent_dims].T) +
-                    A @ (cov[t*n_latent_dims:(t + 1)*n_latent_dims, t*n_latent_dims:(t + 1)*n_latent_dims] +
-                         np.outer(mu[t*n_latent_dims], mu[t*n_latent_dims].T)) @ A.T +
-                    A @ np.outer(mu[t*n_latent_dims], u[t*n_latent_dims].T) @ B.T -
-                    B @ np.outer(u[t*n_latent_dims], mu[t*n_latent_dims].T) +
-                    B @ np.outer(u[t*n_latent_dims], mu[t*n_latent_dims].T) @ A.T +
-                    B @ np.outer(u[t*n_latent_dims], u[t*n_latent_dims]) @ B.T
-                    for t in n_time_steps - 1)
-        B = sum(np.outer(mu[(t+1)*n_latent_dims], u[t*n_latent_dims].T) - A @ np.outer(mu[t*n_latent_dims], u[t*n_latent_dims])
-                for t in n_time_steps-1) @ np.inv(sum(np.outer(u[t*n_latent_dims], u[t*n_latent_dims])for t in n_time_steps-1))
+        q = sum(cov[(t + 1)*nld:(t + 2)*nld, (t + 1)*nld:(t + 2)*nld] +
+                np.outer(mu[(t + 1)*nld], mu[(t + 1)*nld].T) -
+                (cov[(t + 1)*nld:(t + 2)*nld, t*nld:(t + 1)*nld] +
+                np.outer(mu[(t + 1)*nld], mu[t*nld])) @ A.T -
+                np.outer(mu[(t + 1)*nld], u[t*nld].T) @ B.T +
+                A @ (cov[t*nld:(t + 1)*nld, (t + 1)*nld:(t + 2)*nld] +
+                mu[t*nld] @ mu[(t + 1)*nld].T) +
+                A @ (cov[t*nld:(t + 1)*nld, t*nld:(t + 1)*nld] +
+                np.outer(mu[t*nld], mu[t*nld].T)) @ A.T +
+                A @ np.outer(mu[t*nld], u[t*nld].T) @ B.T -
+                B @ np.outer(u[t*nld], mu[t*nld].T) +
+                B @ np.outer(u[t*nld], mu[t*nld].T) @ A.T +
+                B @ np.outer(u[t*nld], u[t*nld]) @ B.T
+                for t in range(n_time_steps - 1))
 
+        B = sum(np.outer(mu[(t+1)*nld], u[t*nld].T) - A @ np.outer(mu[t*nld], u[t*nld])
+                for t in n_time_steps-1) @ np.linalg.inv(sum(np.outer(u[t*nld], u[t*nld])for t in n_time_steps-1))
 
         # Create instance of joint log posterior with determined parameters
-        jll = jllmake(y, n_neurons, n_time_steps, mu, cov, Q, Q0, x0, A, u, G)
+        jll = jllmake(y, n_stimuli_dims, n_neurons, n_time_steps, mu, cov, Q, Q0, x0, A, u, B)
 
         # Second NR minimization to compute C, d (and in principle, D)
 
-        C, d = nr_algo(jll, (C, d))
+        # need to vectorize C for the purpose of gradient descent, thus making a vector (d[i], C[i]), i.e. hessian for
+        # each neuron
+        dC = np.array([np.concatenate(d[i], C[i]) for i in range(n_neurons)])
 
+        djlld = sum(
+                y[t] + np.exp(C @ mu[t*nld:(t+1)*nld] + d + .5 * np.diag(C.T @ cov[t*nld:(t+1)*nld, t*nld:(t+1)*nld] @ C))
+                for t in range(n_time_steps))
+
+        djllC = np.array([
+                sum(
+                y[t][i] * cov[t*nld:(t+1)*nld, t*nld:(t+1)*nld] @ C[i] + mu[t] +
+                np.exp(C[i] @ mu[t*nld:(t+1)*nld] + d[i] + .5 * C[i] @ cov[t*nld:(t+1)*nld, t*nld:(t+1)*nld] @ C[i]) *
+                (u[t] + cov[t*nld:(t+1)*nld, t*nld:(t+1)*nld] @ C[i])
+                for t in range(n_time_steps))
+                for i in range(n_neurons)])
+
+        djlldC = np.array([np.concatenate(djlld[i], djllC[i]) for i in range(n_neurons)])
+
+        Hjll = np.zeros((n_neurons + n_neurons * nld, n_neurons + n_neurons * nld))
+
+        for i in range(n_neurons):
+            Hjll[i * (nld + 1), i * (nld + 1)] = sum(np.exp(C[i] @ mu[t*nld:(t+1)*nld] + d[i] +
+                                                    .5 * C[i] @ cov[t*nld:(t+1)*nld, t*nld:(t+1)*nld] @ C[i])
+                                                    for t in range(n_time_steps))
+
+            Hjll[i * (nld + 1), i * (nld + 1) + 1:(i + 2) * (nld + 1)] = \
+                sum((mu[t] + cov[t, t] @ C[i]) * np.exp(C[i] @ mu[t*nld:(t+1)*nld] + d[i] +
+                    .5 * C[i] @ cov[t*nld:(t+1)*nld, t*nld:(t+1)*nld] @ C[i]) for t in range(n_time_steps))
+
+            Hjll[i * (nld + 1) + 1:(i + 2) * (nld + 1), i * (nld + 1)] = \
+                Hjll[i * (nld + 1), i * (nld + 1) + 1:(i + 2) * (nld + 1)].T
+
+            Hjll[i * (nld + 1) + 1:(i + 2) * (nld + 1), i * (nld + 1) + 1:(i + 2) * (nld + 1)] = np.array([
+                sum(y[t][i] * cov[t, t] @ C[i] + mu[t] +
+                np.outer(mu[t] + cov[t, t] @ C[i], mu[t] + cov[t, t] @ C[i])[i] +
+                np.exp(C[i] @ mu[t*nld:(t+1)*nld] + d[i] + .5 * C[i] @ cov[t*nld:(t+1)*nld, t*nld:(t+1)*nld] @ C[i]) *
+                (u[t] + cov[t*nld:(t+1)*nld, t*nld:(t+1)*nld] @ C[i])
+                for t in range(n_time_steps))
+                ])
+
+        Hjll[n_neurons:, :n_neurons] = Hjll[:n_neurons, n_neurons:].T
+
+        dC = nr_algo(jll, djlldC, Hjll, dC)
+
+        # NEEDS TO BE FIXED !!!!!!!
+        d = dC[:n_neurons]
+        C = dC[n_neurons:].reshape(-1, n_neurons)
+        # !!!!!!!!
--- newton_method.py
+++ newton_method.py
@@ -2,33 +2,32 @@ import numpy as np
 import numdifftools as nd
 
 
-def nr_step(f, x, fold):
+def nr_step(f, df, H, x, fold):
     """
     Performs a single Newton-Raphson step
     :param f: incoming function, receives x as input
     :param x: arguments of function f
+    :param df: derivative of f at point x
+    :param H: hessian of f at point x
     :param fold: past iteration value of f
-    :return: xnew: updated version of x
+    :return: xnew, fnew: updated version of x, f(xnew)
     """
 
-    grad = nd.Gradient(f)(x)
-    hess = nd.Hessian(f)(x)
-    newton_dir = np.linalg.solve(hess, -grad)
-    # newton_dir = -np.dot(np.linalg.inv(hess), grad)
+    newton_dir = np.linalg.solve(H, -df)
     xnew = x + newton_dir
     alpha = .0001
     fnew = f(xnew)  # this can be optimized by saving this value as fold for the next step
 
 
     # First test
-    test = fnew <= fold + alpha * np.dot(grad, xnew - x)
+    test = fnew <= fold + alpha * np.dot(df, xnew - x)
     if test:
-        return xnew
+        return xnew, fnew
 
     # Next test
     gamma1 = 1
     g0 = 1. * fold
-    gprime = np.dot(grad, newton_dir)
+    gprime = np.dot(df, newton_dir)
     g1 = f(x + gamma1 * newton_dir)
 
     gamma2 = -.5 * gprime / (g1 - g0 - gprime)
@@ -39,9 +38,9 @@ def nr_step(f, x, fold):
 
     xnew = x + gamma2 * newton_dir
     fnew = f(xnew)
-    test = fnew <= fold + alpha * grad @ (xnew - x)
+    test = fnew <= fold + alpha * df @ (xnew - x)
     if test:
-        return xnew
+        return xnew, fnew
 
     # Finally go to modeling g as cubic
     while not test:
@@ -58,25 +57,30 @@ def nr_step(f, x, fold):
             gamma3 = .5 * gamma1
         xnew = x + gamma3 * newton_dir
         fnew = f(xnew)
-        test = fnew <= fold + alpha * grad @ (xnew - x)
+
         gamma1 = 1. * gamma2
         gamma2 = 1. * gamma3
+
+        test = fnew <= fold + alpha * df @ (xnew - x)
 
     return xnew, fnew
 
 
-def nr_algo(f, x, threshold=.00001):
+def nr_algo(f, df, H, x, threshold=.00001):
     """
     Performs the Newton-Raphson optimization method
     :param f: function to be optimized
     :param x: starting location
+    :param df: derivative at point x
+    :param H: Hessian at point x
     :param threshold: cutoff value, improvements smaller that this value are inconsequential
     :return: location of optimum
     """
 
     fold = f(x)
+    cont = True
     while cont:
-        x, fnew = nr_step(f, x, fold)
+        x, fnew = nr_step(f, df(x), H(x), x, fold)
         if fnew - fold < threshold:
             cont = False
         fold = 1. * fnew
