66661c77c2d9682257b860662a631c71100d33c1
Bishara Korkor <Bishara.Korkor@gmail.com>
2017-12-07 14:47:11 -0800

    added test module, fully functioning plds model"

--- pldstest.py
+++ pldstest.py
@@ -5,8 +5,10 @@ import scipy.sparse as scsp
 import scipy.sparse.linalg as splin
 import numpy as np
 from newton_method import nr_algo
-import sksparse.cholmod as chol
-# import matplotlib.pyplot as plt
+import scipy.optimize as sciop
+import numdifftools as nd
+
+import matplotlib.pyplot as plt
 
 
 def kd(i, j):
@@ -69,7 +71,7 @@ def logposteriorderivative(y, C, d, A, B, q, q0, m0, u, nts, nn, nsd, nld):
 
         for t in range(1, nts-1):
             df[t*nld:(t+1)*nld] = - C.T @ y[t*nn:(t+1)*nn] \
-                                     + C.T @ np.exp(C @ x[t*nld:(t+1)*nld] + d) \
+                                  + C.T @ np.exp(C @ x[t*nld:(t+1)*nld] + d) \
                                   - ATQinv @ x[(t+1)*nld:(t+2)*nld] \
                                   + ATQinvA @ x[t*nld:(t+1)*nld] \
                                   + ATQinvB @ u[t*nsd:(t+1)*nsd] \
@@ -134,16 +136,16 @@ def jllDerivative(nn, nld, mu, cov, nts, y):
         d = dC[::nld+1]
         C = np.array([dC[i*(nld+1)+1:(i+1)*(nld+1)] for i in range(nn)])
 
-        djlld = np.zeros(nn)
+        djlld = np.empty(nn)
         for i in range(nn):
             djlld[i] = sum(-y[t*nn+i] \
-                           + np.exp(C[i] @ mu[t*nld:(t+1)*nld] + d[i] + .5 * (C[i] @ cov[t*nld:(t+1)*nld, t*nld:(t+1)*nld] @ C[i]))
-                           for t in range(nts-1))
+                    + np.exp(C[i] @ mu[t*nld:(t+1)*nld] + d[i] + .5 * (C[i] @ cov[t*nld:(t+1)*nld, t*nld:(t+1)*nld] @ C[i]))
+                    for t in range(nts-1))
 
         djllC = [sum(-y[t*nn+i] * mu[t*nld:(t+1)*nld] + \
-                     np.exp(C[i] @ mu[t*nld:(t+1)*nld] + d[i] + .5 * (C[i] @ cov[t*nld:(t+1)*nld, t*nld:(t+1)*nld] @ C[i])) * \
-                     (mu[t*nld:(t+1)*nld] + cov[t*nld:(t+1)*nld, t*nld:(t+1)*nld] @ C[i])
-                     for t in range(nts-1)) for i in range(nn)]
+                    np.exp(C[i] @ mu[t*nld:(t+1)*nld] + d[i] + .5 * (C[i] @ cov[t*nld:(t+1)*nld, t*nld:(t+1)*nld] @ C[i])) * \
+                    (mu[t*nld:(t+1)*nld] + cov[t*nld:(t+1)*nld, t*nld:(t+1)*nld] @ C[i])
+                    for t in range(nts-1)) for i in range(nn)]
 
         djlldC = np.empty(nn*(nld+1))
         for i in range(nn):
@@ -182,74 +184,80 @@ def jllHessian(nn, nld, mu, cov, nts, y):
     return f
 
 
-def blocktridiaginv(h, bw, nb):
-    U = chol.cholesky(h).L().toarray().T
-    S = np.zeros_like(h.toarray())
-    S[-bw:,-bw:] = np.linalg.inv(U[-bw:,-bw:].T @ U[-bw:,-bw:])
-    for t in np.arange(nb-2, -1, -1):
-        S[t*bw:(t+1)*bw, (t+1)*bw:(t+2)*bw] = -np.linalg.inv(U[t*bw:(t+1)*bw, t*bw:(t+1)*bw]) @ U[t*bw:(t+1)*bw, (t+1)*bw:(t+2)*bw] @ S[(t+1)*bw:(t+2)*bw, (t+1)*bw:(t+2)*bw]
-        S[t*bw:(t+1)*bw, t*bw:(t+1)*bw] = np.linalg.inv(U[t*bw:(t+1)*bw, t*bw:(t+1)*bw].T @ U[t*bw:(t+1)*bw, t*bw:(t+1)*bw]) \
-                                              - S[t*bw:(t+1)*bw, (t+1)*bw:(t+2)*bw] @ (np.linalg.inv(U[t*bw:(t+1)*bw, t*bw:(t+1)*bw]) @ U[t*bw:(t+1)*bw, t*bw:(t+1)*bw]).T
-        S[(t+1)*bw:(t+2)*bw, t*bw:(t+1)*bw] = S[t*bw:(t+1)*bw, (t+1)*bw:(t+2)*bw].T
-    return S
+def blockdiaginv(m, nb, bw):
+    minv = np.zeros_like(m)
+    for b in range(nb):
+        minv[b*bw:(b+1)*bw, b*bw:(b+1)*bw] = np.linalg.inv(m[b*bw:(b+1)*bw, b*bw:(b+1)*bw])
+    return minv
 
 
 def laplace_approximation(f, df, hf, x, nts, nld):
     # use NR algorithm to compute minimum of log-likelihood
     x = nr_algo(f, df, hf, x)
+    # x = sciop.minimize(f, x0, jac=df, hess=hf, method='Newton-CG').x
     # negative inverse of Hessian is covariance matrix
-    # covariance = splin.inv(hf(x)).toarray()
-    covariance = blocktridiaginv(hf(x).toarray(), nld)
+    covariance = blockdiaginv(hf(x), nts, nld)
     return x, covariance
 
 
 if __name__ == "__main__":
     # load data
     print('loading data')
-    nts = 1000
-    nn = 300  # number of neurons
-    nld = 5  # number of latent dimensions
+
+    nts = 10
+    nn = 6
+    nld = 3
     nsd = 4
-    frameHz = 10  # frames per seconds
-    data = scio.loadmat('data/compiled_dF033016.mat')
-    y = data['behavdF'].flatten()[:nts*nn]
-    onset = np.array(data['onsetFrame'].T[0], np.int8)
-    resptime = data['resptime'].T[0]
-    correct = data['correct'][0]
-    orient = np.array(data['orient'][0], np.int8)
-    location = np.array((data['location'][0]+1)//2, np.int8)
-    u = np.zeros((nts, nsd))
-    for ot, rt, cor, ori, loc in zip(onset, resptime, correct, orient, location):
-        # compute what u should be here
-        u[int(ot):ot+int((rt+2.75+(4.85-2.75)*(1-cor))*frameHz)] = np.array([ori*loc, (1-ori)*loc, ori*(1-loc), (1-ori)*(1-loc)], np.int)
-    u = u.flatten()
+    y = np.load('ytest.npy')
+    u = np.zeros(nts * nsd)
 
     print('variable initialization')
     # Initialize parameters to random values
-    C = np.random.rand(nn, nld)
-    d = np.random.rand(nn)
-    m0 = np.random.rand(nld)
-    A = np.random.rand(nld, nld)
-    q0 = np.random.rand(nld, nld)
-    q0 = q0 @ q0.T
-    q = np.random.rand(nld, nld)
-    q = q @ q.T
-    B = np.random.rand(nld, nsd)
-    mu = np.random.rand(nld*nts)
+    C = np.load('Ctest.npy')
+    # C = np.random.randn(nn, nld)
+    d = np.load('dtest.npy')
+    # d = np.random.randn(nn)
+    m0 = np.zeros(nld)
+    A = np.zeros((nld, nld))
+    # A = np.load('Atest.npy')
+    q0 = np.identity(nld)
+    q = np.identity(nld)
+    B = np.zeros((nld, nsd))
+    mu = np.load('xtest.npy') # + np.random.randn(nld*nts)
+    xgen = np.load('xtest.npy')
+    Cgen = np.load('Ctest.npy')
+    dgen = np.load('dtest.npy')
+
+    rsquaredC = [1 - np.mean((C-Cgen)**2 / Cgen**2)]
+    rsquaredd = [1 - np.mean((d-dgen)**2 / dgen**2)]
+    rsquaredx = [1 - np.mean((mu-xgen)**2 / xgen**2)]
 
     print('begin training')
-    max_epochs = 5
+    max_epochs = 25
     for epoch in range(max_epochs):
         print('epoch {}'.format(epoch))
         print('performing laplace approximation')
         # perform laplace approximation on log-posterior with Newton-Raphson optimization to find mean and covariance
+
+        lp = logposterior(y, C, d, A, B, q, q0, m0, u, nts, nn, nsd)
+
+        numdif = np.empty_like(mu)
+        epsilon = .0001
+        for dim in range(len(mu)):
+            delt = np.zeros_like(mu)
+            delt[dim] = epsilon
+            numdif[dim] = (lp(mu + delt) - lp(mu - delt))/(2*epsilon)
+
         mu, cov = laplace_approximation(logposterior(y, C, d, A, B, q, q0, m0, u, nts, nn, nsd),
                                         logposteriorderivative(y, C, d, A, B, q, q0, m0, u, nts, nn, nsd, nld),
                                         logposteriorhessian(y, C, d, A, B, q, q0, m0, u, nts, nn, nsd, nld),
                                         mu, nts, nld)
 
+        print("rsquared of mu and xgen = ", 1 - np.mean((mu-xgen)**2 / xgen**2))
+
         print('assigning analytic expressions')
         # Use analytic expressions to compute parameters m0, Q, Q0, A, B
+
         m0 = mu[:nld]
         q0 = cov[:nld, :nld]
 
@@ -284,6 +292,15 @@ if __name__ == "__main__":
             dC += list(C[i])
         dC = np.array(dC)
 
+        jl = jointloglikelihood(y, nsd, nn, nts, mu, cov, q, q0, m0, A, u, B)
+
+        numdif = np.empty_like(dC)
+        epsilon = .0001
+        for dim in range(len(dC)):
+            delt = np.zeros_like(dC)
+            delt[dim] = epsilon
+            numdif[dim] = (jl(dC + delt) - jl(dC - delt))/(2*epsilon)
+
         dC = nr_algo(jointloglikelihood(y, nsd, nn, nts, mu, cov, q, q0, m0, A, u, B),
                      jllDerivative(nn, nld, mu, cov, nts, y),
                      jllHessian(nn, nld, mu, cov, nts, y),
@@ -291,6 +308,13 @@ if __name__ == "__main__":
 
         for i in range(nn):
             d[i] = dC[i*(nld+1)]
-            C[i] = dC[i*(nld+1) + 1:(i+1)*(nld+1)]
+            C[i] = dC[i*(nld+1)+1:(i+1)*(nld+1)]
 
+        rsquaredC.append(1 - np.mean((C-Cgen)**2 / Cgen**2))
+        rsquaredd.append(1 - np.mean((d-dgen)**2 / dgen**2))
+        rsquaredx.append(1 - np.mean((mu-xgen)**2 / xgen**2))
 
+    plt.plot(rsquaredC, 'k-')
+    plt.plot(rsquaredd, 'b-')
+    plt.plot(rsquaredx, 'g-')
+    plt.show()
