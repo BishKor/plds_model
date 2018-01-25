eccba592ee50c4c6b0a16349c3b7707a62b6cc4d
Bishara Korkor <Bishara.Korkor@gmail.com>
2018-01-24 16:30:32 -0800

    dense and sparse modality included

--- scripts/newton_method.py
+++ scripts/newton_method.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.sparse.linalg as splin
 
 
-def nr_step(f, df, H, x, fold, alpha=.0001, tolx=.0001, stpmax=1000):
+def nr_step(f, df, H, x, fold, mode='sparse', alpha=.0001, tolx=.0001, stpmax=1000):
     """
     Performs a single Newton-Raphson step
     :param f: incoming function, receives x as input
@@ -12,8 +12,11 @@ def nr_step(f, df, H, x, fold, alpha=.0001, tolx=.0001, stpmax=1000):
     :param fold: past iteration value of f
     :return: xnew, fnew: updated version of x, f(xnew)
     """
-    newton_dir = splin.spsolve(H, -df)
-    # newton_dir = np.linalg.solve(H, -df)
+
+    if mode == 'sparse':
+        newton_dir = splin.spsolve(H, -df)
+    elif mode == 'dense':
+        newton_dir = np.linalg.solve(H, -df)
 
     if np.linalg.norm(newton_dir) > stpmax:
         print("rescaling newton direction")
@@ -70,7 +73,7 @@ def nr_step(f, df, H, x, fold, alpha=.0001, tolx=.0001, stpmax=1000):
         lam = max(tmplam, .1*lam)
 
 
-def nr_algo(f, df, h, x):
+def nr_algo(f, df, h, x, mode='sparse'):
     """
     Performs the Newton-Raphson optimization method
     :param f: function to be optimized
@@ -86,7 +89,7 @@ def nr_algo(f, df, h, x):
     iter = 0
     while cont:
         iter += 1
-        x, fnew, check = nr_step(f, df(x), h(x), x, fold)
+        x, fnew, check = nr_step(f, df(x), h(x), x, fold, mode=mode)
         # check for convergence on function values
         # I'm not sure how this should be implemented
         # check for grad of f zero , i.e., spurious convergence
